6d9dd83faaffe8fdeadcbfd61468bda105f67a82
cdavis448 <cdavis448@lsr7.net>
2018-12-14 09:58:04 -0500

    DEATH

--- slugi.py
+++ slugi.py
@@ -12,73 +12,84 @@ direction = "right"
 blank = (0,0,0)
 vegan=[]
 score = 0
+pause = 0.15
+dead = True
 # Functions ---------------------------
 def draw_slug():
     for i in slug:
         sense.set_pixel(i[0],i[1],w)
 def move():
     global score
+    global pause
+    global dead
     remove = True
     while True:
-        if len(vegan) < 5:
-            make_veg()
-        last = slug[-1]
-        first = slug[0]
-        next = list(last)
-        if direction == "right":
-            if last[0] + 1 == 8:
-                next[0] = 0
-            else:
-                next[0] = last[0] + 1
-        elif direction == "left":
-            if last[0] - 1 == -1:
+        if True == True:
+            last = slug[-1]
+            first = slug[0]
+            next = list(last)
+            if direction == "right":
+                if last[0] + 1 == 8:
+                    next[0] = 0
+                else:
+                    next[0] = last[0] + 1
+            elif direction == "left":
+                if last[0] - 1 == -1:
 
-                next[0] = 7
-            else:
-                next[0] = last[0] - 1
-        elif direction == "down":
-            if last[1] + 1 == 8:
-                next[1] = 0
-            else:
-                next[1] = last[1] + 1
-        elif direction == "up":
-            if last[1] - 1 == -1:
-                next[1] = 7
-            else:
-                next[1] = last[1] - 1
-        slug.append(next)
-        sense.set_pixel(next[0], next[1], w)
-        if remove == True:
-            sense.set_pixel(first[0], first[1], blank)
-            slug.remove(first)
-        draw_slug()
+                    next[0] = 7
+                else:
+                    next[0] = last[0] - 1
+            elif direction == "down":
+                if last[1] + 1 == 8:
+                    next[1] = 0
+                else:
+                    next[1] = last[1] + 1
+            elif direction == "up":
+                if last[1] - 1 == -1:
+                    next[1] = 7
+                else:
+                    next[1] = last[1] - 1
+            slug.append(next)
+            sense.set_pixel(next[0], next[1], w)
+            if remove == True:
+                sense.set_pixel(first[0], first[1], blank)
+                slug.remove(first)
+            draw_slug()
         
-        if [next[0],next[1]] in vegan:
-            vegan.remove([next[0],next[1]])
-            score = score + 1
-            if score % 2 == 0:
-                remove = False
+            if [next[0],next[1]] in slug:
+                dead = False
+            if [next[0],next[1]] in vegan:
+                vegan.remove([next[0],next[1]])
+                score = score + 1
+                if score % 5 == 0:
+                    remove = False
+                elif score % 10 == 0:
+                    if pause >= 0.10:
+                        pause = pause - 0.01
             else:
                 remove = True
-        sleep(0.15)
+            sleep(pause)
+            if len(vegan) < 5:
+                make_veg()
 def joystick_moved(event):
     global direction
     direction = event.direction
 def make_veg():
-    x = randint(0,7)
-    y = randint(0,7)
-    while [x,y] in slug:
+    if randint(1,10) / 10 > 7/10:
         x = randint(0,7)
         y = randint(0,7)
-        sense.set_pixel(x,y,r)
-    vegan.append([x,y])
+        food = [x,y]
+        if food in slug:
+            if len(vegan) < 5:
+                x = randint(0,7)
+                y = randint(0,7)
+        sense.set_pixel(x,y,r)     
+        vegan.append([x,y])
 
 # Main program ------------------------
 sense.clear()
 draw_slug
 sleep(0.1)
 sense.stick.direction_any = joystick_moved
-move()
-make_veg()
 move()
 
